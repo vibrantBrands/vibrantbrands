f88af3aabc3694940bbc5c523d1a51a44102bb8d
Benjamin Ratliff <ben@2bitcreative.com>
2020-06-04 12:05:01 -0400

    updates

---
+++ page_theme.php
@@ -0,0 +1,126 @@
+<?php
+namespace Application\Theme\VibrantBrands;
+
+use Concrete\Core\Area\Layout\Preset\Provider\ThemeProviderInterface;
+use Concrete\Core\Page\Theme\Theme;
+
+class PageTheme extends Theme implements ThemeProviderInterface
+{
+    public function registerAssets()
+    {
+        $this->requireAsset('css', 'font-awesome');
+        $this->requireAsset('javascript', 'jquery');
+    }
+
+    protected $pThemeGridFrameworkHandle = 'bootstrap3';
+
+    public function getThemeName()
+    {
+        return t('Vibrant Redux');
+    }
+
+    public function getThemeDescription()
+    {
+        return t('A Vibrant Page Theme');
+    }
+
+    /**
+     * @return array
+     */
+    public function getThemeBlockClasses()
+    {
+        return [
+            'feature' => ['feature-home-page'],
+            'page_list' => [
+                'recent-blog-entry',
+                'blog-entry-list',
+                'page-list-with-buttons',
+                'block-sidebar-wrapped',
+            ],
+            'next_previous' => ['block-sidebar-wrapped'],
+            'share_this_page' => ['block-sidebar-wrapped'],
+            'content' => [
+                'block-sidebar-wrapped',
+                'block-sidebar-padded',
+            ],
+            'date_navigation' => ['block-sidebar-padded'],
+            'topic_list' => ['block-sidebar-wrapped'],
+            'testimonial' => ['testimonial-bio'],
+            'image' => [
+                'image-right-tilt',
+                'image-circle',
+            ],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getThemeAreaClasses()
+    {
+        return [
+            'Page Footer' => ['area-content-accent'],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getThemeDefaultBlockTemplates()
+    {
+        return [
+            'calendar' => 'bootstrap_calendar.php',
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getThemeResponsiveImageMap()
+    {
+        return [
+            'large' => '900px',
+            'medium' => '768px',
+            'small' => '0',
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getThemeEditorClasses()
+    {
+        return [
+            ['title' => t('Subtle Link'), 'menuClass' => '', 'spanClass' => 'subtle-link', 'forceBlock' => '-1'],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getThemeAreaLayoutPresets()
+    {
+        $presets = [
+            [
+                'handle' => 'left_sidebar',
+                'name' => 'Left Sidebar',
+                'container' => '<div class="row"></div>',
+                'columns' => [
+                    '<div class="col-sm-4"></div>',
+                    '<div class="col-sm-8"></div>',
+                ],
+            ],
+            [
+                'handle' => 'right_sidebar',
+                'name' => 'Right Sidebar',
+                'container' => '<div class="row"></div>',
+                'columns' => [
+                    '<div class="col-sm-8"></div>',
+                    '<div class="col-sm-4"></div>',
+                ],
+            ],
+        ];
+
+        return $presets;
+    }
+}
